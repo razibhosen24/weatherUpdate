93242e5ce162485d7bdb6b21fe4bd15101a7b924
razibhosen24 <razibhosen24@gmail.com>
2025-06-29 20:38:21 +0600

    weatherUpadet App

---
+++ js/script.js
@@ -0,0 +1,35 @@
+// https://api.openweathermap.org/data/2.5/weather?q=Khulna&appid=ad1987b812406922db28731ceb6db4c5&units=metric
+
+const apikey = "ad1987b812406922db28731ceb6db4c5";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather img");
+
+async function checkWeather(city){
+    const response = await fetch( apiUrl + city + `&appid=${apikey}`);
+    const data = await response.json();
+    console.log(data);
+
+    document.querySelector(".city").innerText = data.name;
+    document.querySelector(".temp").innerText = Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerText = data.main.humidity + "%";
+    document.querySelector(".wind").innerText = data.wind.speed + " km/h";
+
+    if(data.weather[0].main == "Clouds"){
+        weatherIcon.src = "img/clouds.png";
+    } else if(data.weather[0].main == "Clear"){
+        weatherIcon.src = "img/clear.png";
+    } else if(data.weather[0].main == "Rain"){
+        weatherIcon.src = "img/rain.png";
+    } else if(data.weather[0].main == "Drizzle"){
+        weatherIcon.src = "img/drizzle.png";
+    } else if(data.weather[0].main == "Mist"){
+        weatherIcon.src = "img/mist.png";
+    }
+
+    
+}
+searchBtn.addEventListener("click", () => { 
+    checkWeather(searchBox.value);
+});
